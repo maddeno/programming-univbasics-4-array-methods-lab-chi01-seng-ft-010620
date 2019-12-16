575eb677e2249b6a673b0e8f818e750b429b5ae5
Olivia Madden <madden.livi@gmail.com>
2019-12-16 22:52:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,8 @@ end
 
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.sort["I", "am", "really", "learning", "arrays!", "wow"]
+  array.sort
+  =>["I", "am", "really", "learning", "arrays!", "wow"]
 end
 
 def using_reverse(array)
